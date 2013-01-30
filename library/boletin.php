8da87bc333cbedd276b81e98bd40f5a5b3394038
Marco Godínez <markotom@gmail.com>
2013-01-30 02:38:27 -0600

    Remove support of bulletin to child blogs

--- library/boletin.php
+++ library/boletin.php
@@ -1,47 +1,56 @@
 <?php
 
-register_nav_menus(                      
-  array( 'boletin' => __( 'Boletín', 'pleromabootstrap' ) )  // register menu
-);
+global $blog_id;
 
-function pleroma_boletin_nav() {
-  wp_nav_menu(array( 
-      'theme_location'  => 'boletin'                      // location in theme
-    , 'container'       => false                          // remove nav container
-    , 'depth'           => -1                             // depth of the nav
-    , 'items_wrap'      => '<ul class="nav">%3$s</ul>'    // adapted to twitter bootstrap (not yet dropdown)    // 
-    , 'fallback_cb'     => '__return_false'               // avoiding fallback default function
-  ));
-  return true; // required if you enjoy a hook
-}
-
-function load_cat_parent_template()
+// no support for child blogs
+if( 1 == $blog_id )
 {
-    global $wp_query;
 
-    if ($wp_query->is_page)
-        return true; // apply to category & single
+  register_nav_menus(                      
+    array( 'boletin' => __( 'Boletín', 'pleromabootstrap' ) )  // register menu
+  );
 
-    // get current object
-    $cat = $wp_query->get_queried_object();
+  function pleroma_boletin_nav() {
+    wp_nav_menu(array( 
+        'theme_location'  => 'boletin'                      // location in theme
+      , 'container'       => false                          // remove nav container
+      , 'depth'           => -1                             // depth of the nav
+      , 'items_wrap'      => '<ul class="nav">%3$s</ul>'    // adapted to twitter bootstrap (not yet dropdown)    // 
+      , 'fallback_cb'     => '__return_false'               // avoiding fallback default function
+    ));
+    return true; // required if you enjoy a hook
+  }
 
-    // trace back the parent hierarchy and locate a template
-    while ($cat && !is_wp_error($cat)) {
-        $template = TEMPLATEPATH . "/category-{$cat->slug}.php";
+  function load_cat_parent_template()
+  {
+      global $wp_query;
 
-        // overriding default style
-        wp_register_style( 'pleroma',    get_stylesheet_directory_uri() . '/css/style-boletin.css' );
+      if ($wp_query->is_page)
+          return true; // apply to category & single
 
-        // overriding primary nav
-        add_filter('pleroma_primary_nav', 'pleroma_boletin_nav');
+      // get current object
+      $cat = $wp_query->get_queried_object();
 
-        if (file_exists($template)) {
-            load_template($template);
-            exit;
-        }
+      // trace back the parent hierarchy and locate a template
+      while ($cat && !is_wp_error($cat)) {
+          $template = TEMPLATEPATH . "/category-{$cat->slug}.php";
 
-        $cat = $cat->parent ? get_category($cat->parent) : false;
-    }
+          // overriding default style
+          wp_register_style( 'pleroma',    get_stylesheet_directory_uri() . '/css/style-boletin.css' );
+
+          // overriding primary nav
+          add_filter('pleroma_primary_nav', 'pleroma_boletin_nav');
+
+          if (file_exists($template)) {
+              load_template($template);
+              exit;
+          }
+
+          $cat = $cat->parent ? get_category($cat->parent) : false;
+      }
+  }
+
+  add_action('template_redirect', 'load_cat_parent_template');
+
+
 }
-
-add_action('template_redirect', 'load_cat_parent_template');
