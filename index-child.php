2e390ec224b7c710ce875a85ae9696a74e4429d3
Marco Godínez <markotom@gmail.com>
2013-02-01 13:30:24 -0600

    Added primary nav with fallback in sidebar 1 (multisites)

--- index-child.php
+++ index-child.php
@@ -2,15 +2,6 @@
 
         <div class="row-fluid">
           <div class="span3">
-            <?php
-              wp_nav_menu(array( 
-                  'theme_location'  => 'primary'                  
-                , 'container'       => false                      
-                , 'depth'           => 2                          
-                , 'items_wrap'      => '<ul class="nav nav-tabs nav-stacked">%3$s</ul>'
-                , 'fallback_cb'     => 'pleroma_nav_menu_args'
-              ));
-            ?>
             <?php get_sidebar(1); // sidebar page 1 ?>
           </div>
           <div class="span9">
--- sidebar-1.php
+++ sidebar-1.php
@@ -1,3 +1,16 @@
+          <?php global $blog_id; ?>
+          <?php
+            if ( $blog_id > 1 )
+            {
+              wp_nav_menu(array( 
+                  'theme_location'  => 'primary'                  
+                , 'container'       => false                      
+                , 'depth'           => 2                          
+                , 'items_wrap'      => '<ul class="nav nav-tabs nav-stacked">%3$s</ul>'
+                , 'fallback_cb'     => 'pleroma_nav_menu_args'
+              ));
+            }
+          ?>
           <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
             <?php dynamic_sidebar( 'sidebar-1' ); ?>
