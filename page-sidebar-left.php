6a59eff0bad4641d2a20e018abdc03140da138bf
Marco Godínez <markotom@gmail.com>
2013-02-04 23:54:33 -0600

    Sidebar left for pages of child blogs

--- page-sidebar-left.php
+++ page-sidebar-left.php
@@ -5,11 +5,20 @@ Template Name: Páginas (sidebar izquierdo)
 ?>
 
 <?php get_header(); ?>
+<?php global $blog_id; ?>
 
         <div class="row-fluid">
 
           <div class="span3">
-            <?php get_sidebar('page-1'); // sidebar page 1 ?>
+            <?php
+              if( $blog_id > 1 )
+              {
+                get_sidebar(1);        // sidebar 1
+              } else
+              {
+                get_sidebar('page-1'); // sidebar page 1
+              }
+            ?>
           </div><!-- /.span3 -->
 
           <div id="main" role="main" class="span9">
