ef4ca0a2f778ec4e91de881ea531a66251213e97
Marco Godínez <markotom@gmail.com>
2013-02-04 23:57:38 -0600

    Sidebar right for pages of child blogs

--- page-sidebar-right.php
+++ page-sidebar-right.php
@@ -5,6 +5,7 @@ Template Name: Páginas (sidebar derecho)
 ?>
 
 <?php get_header(); ?>
+<?php global $blog_id; ?>
 
         <div class="row-fluid">
 
@@ -13,7 +14,15 @@ Template Name: Páginas (sidebar derecho)
           </div><!-- /.span9 -->
 
           <div class="span3">
-            <?php get_sidebar('page-2'); // sidebar page 2 ?>
+            <?php
+              if( $blog_id > 1 )
+              {
+                get_sidebar(2);        // sidebar 2
+              } else
+              {
+                get_sidebar('page-2'); // sidebar page 2
+              }
+            ?>
           </div><!-- /.span3 -->
 
         </div><!-- /.row-fluid -->
