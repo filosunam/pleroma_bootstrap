d806af02be8acf97639153c99a6c183145ff8c51
Marco Godínez <markotom@gmail.com>
2013-02-21 12:45:49 -0600

    Remove title if is home

--- page-single.php
+++ page-single.php
@@ -2,7 +2,7 @@
 
             <article itemscope id="post-<?php the_ID(); ?>" role="article" itemtype="http://schema.org/BlogPosting">
               
-              <?php if (!is_front()) { ?>
+              <?php if (!is_home()) { ?>
               <header class="article-header">
               
                 <h1 class="h3 lead" itemprop="headline"><?php the_title(); ?></h1>
