de3c6b349b3f117d6085db87b8d760fde8867e61
Marco Godínez <markotom@gmail.com>
2013-04-02 11:31:26 -0600

    Change transition of home slider

--- index-parent.php
+++ index-parent.php
@@ -17,7 +17,7 @@
               if ( $query->post_count > 0 ) :
 
           ?>
-            <div id="myCarousel" class="carousel carousel-fade slide">
+            <div id="myCarousel" class="carousel slide">
               <div class="carousel-inner">
                 <?php
 
