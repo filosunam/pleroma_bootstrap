515ca0ff83d045fffa8ad48cc64b091fdfb1453c
Marco Godínez <markotom@gmail.com>
2013-07-23 12:57:48 -0500

    Changed home coditional (less code)

--- index.php
+++ index.php
@@ -1,28 +1,17 @@
 <?php 
 
-  if( is_multisite() )
+  if( !is_multisite() ||  1 == get_current_blog_id() )
   {
-
-    if ( 1 == get_current_blog_id() ) 
-    {
     
-      // www.filos.unam.mx
-      get_template_part( 'index', 'parent' );
-    
-    } else 
-    {
-
-      // *.filos.unam.mx
-      get_template_part( 'index', 'child' );
-    
-    }
-
-  } else
-  {
-
     // www.filos.unam.mx
     get_template_part( 'index', 'parent' );
+    
+  } else 
+  {
 
+    // *.filos.unam.mx
+    get_template_part( 'index', 'child' );
+  
   }
 
 ?>
