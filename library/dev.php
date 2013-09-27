5421cb7c91be4a9aa1cd39fbb9c2a7d92f0580ea
Marco Godínez <markotom@gmail.com>
2013-09-26 21:47:37 -0500

    Forma correcta de registrar el script livereload
    http://wordpress.org/support/topic/notice-wp_register_script-was-called-
    incorrectly

--- library/dev.php
+++ library/dev.php
@@ -3,8 +3,12 @@
   if ( $_SERVER["SERVER_ADDR"] == '127.0.0.1' ) {
 
     // livereload for faster web development
-    wp_register_script('livereload', 'http://localhost:35729/livereload.js' );
-    wp_enqueue_script('livereload');
+    function register_livereload() {
+      wp_register_script( 'livereload', 'http://localhost:35729/livereload.js' );
+      wp_enqueue_script('livereload');
+    }
+
+    add_action('init', 'register_livereload');
 
   }
 
