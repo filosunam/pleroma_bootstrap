845e1e082521dfc6262d12ed1da66326e568c896
Marco Godínez <markotom@gmail.com>
2013-09-27 13:42:09 -0500

    Se añadió favicon

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- icons & favicons -->
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
   
   <!-- pingback -->
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
